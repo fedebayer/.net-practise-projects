b42284a3e614710d94557ed61b05acc01d1011fb
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-06-10 18:44:06 -0300

    Added province model

---
+++ ProvincesApplication/Models/ProvinciaModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace MVCApplication.Models
+{
+    public class ProvinciaModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Descripcion { get; set; }
+    }
+
+    public static class ProvinciaValidations
+    {
+        public static ValidationResult ValidateId(int id)
+        {
+            public static ValidationResult IsValidIdProvincia(int value)
+            {
+                if (value > 0)
+                {
+                    return ValidationResult.Success;
+                }
+                return new ValidationResult("El Id de la Provincia debe ser mayor a cero");
+            }
+        }
+    }
+}
+}
