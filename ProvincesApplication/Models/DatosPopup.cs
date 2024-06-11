c87582e946a4f8b9854c925fdaf5fd8ff6902482
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-06-11 18:03:41 -0300

    Added pop-up-data model

---
+++ ProvincesApplication/Models/DatosPopup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApplication.Models
+{
+    public class DatosPopup
+    {
+        public bool InSitu { get; set; }
+        public bool Tercerizado { get; set; }
+
+        public IList<TipoTrabajo> TiposTrabajo { get; set; }
+        public int IdTipoTrabajo { get; set; }
+    }
+
+    public class TipoTrabajo
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
