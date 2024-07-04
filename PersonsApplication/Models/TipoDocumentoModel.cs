e12a3648496b371b26e8d691493d89143151bd39
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-07-04 18:02:13 -0300

    Added model/document type

---
+++ PersonsApplication/Models/TipoDocumentoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCIntegrador1.Models
+{
+    public class TipoDocumentoModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo TipoDoc es obligatorio.")]
+        [StringLength(15, ErrorMessage = "El campo Nombre no puede tener mas de 15 caracteres.")]
+        public string TipoDoc { get; set; }
+    }
+}
