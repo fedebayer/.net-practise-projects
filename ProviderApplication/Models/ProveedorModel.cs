764157453708c86fc0f0df722c7b8f00a2981807
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-07-31 18:09:57 -0300

    Added provider model

---
+++ ProviderApplication/Models/ProveedorModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCIntegrador2.Models
+{
+    public class ProveedorModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Nombre", ResourceType = typeof(Recursos.Global))]
+        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El campo Nombre no puede tener mas de 100 caracteres.")]
+        public string Nombre { get; set; }
+        [Display(Name = "Domicilio", ResourceType = typeof(Recursos.Global))]
+        [Required(ErrorMessage = "El campo Domicilio es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El campo Domicilio no puede tener mas de 100 caracteres.")]
+        public string Domicilio { get; set; }
+        [Display(Name = "Localidad", ResourceType = typeof(Recursos.Global))]
+        [Required(ErrorMessage = "El campo Localidad es obligatorio.")]
+        public int IdLocalidad { get; set; }
+        [Display(Name = "Provincia", ResourceType = typeof(Recursos.Global))]
+        [Required(ErrorMessage = "El campo Provincia es obligatorio.")]
+        public int IdProvincia { get; set; }
+
+        public LocalidadModel Localidad { get; set; }
+
+        public ProvinciaModel Provincia { get; set; }
+    }
+}
