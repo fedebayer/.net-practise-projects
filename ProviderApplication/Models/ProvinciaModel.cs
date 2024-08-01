65bcf49a8873febccb4e64009cb6d6808f6ef3c5
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-08-01 18:10:34 -0300

    Added province model

---
+++ ProviderApplication/Models/ProvinciaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCIntegrador2.Models
+{
+    public class ProvinciaModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Provincia", ResourceType=typeof(Recursos.Global))]
+        [Required(ErrorMessage = "El campo Provincia es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El campo Provincia no puede tener mas de 100 caracteres.")]
+        public string Provincia { get; set; }
+    }
+}
