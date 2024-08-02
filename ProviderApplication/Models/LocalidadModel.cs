62f089db36fabde22b7ec15cf3e680e9acfaedd0
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-08-02 20:19:12 -0300

    Added location model

---
+++ ProviderApplication/Models/LocalidadModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCIntegrador2.Models
+{
+    public class LocalidadModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Localidad", ResourceType = typeof(Recursos.Global))]
+        [Required(ErrorMessage = "El campo Localidad es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El campo Localidad no puede tener mas de 100 caracteres.")]
+        public string Localidad { get; set; }
+        [Display(Name = "Provincia", ResourceType = typeof(Recursos.Global))]
+        [Required(ErrorMessage = "El campo Provincia es obligatorio.")]
+        public int IdProvincia { get; set; }
+
+        public LocalidadModel Provincia { get; set; }
+    }
+}
