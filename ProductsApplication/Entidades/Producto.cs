b401b1c078b89879c1fca11b360e7bc3777f03e9
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-08-21 18:46:02 -0300

    Added entity/product

---
+++ ProductsApplication/Entidades/Producto.cs
@@ -0,0 +1,41 @@
+﻿/*FB: Se soluciona error de que todos los precios sean 0 cuando no es asi, habia un error de escritura en la linea 22 decia 'PRCIO' y era 'PRECIO'*/
+
+using System.Data;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public int Precio { get; set; }
+
+        public int IdCategoria { get; set; }
+
+        public string DescripcionCategoria { get; set; }
+
+        public Producto FromDataRow(DataRow dataRow)
+        {
+            Producto entidad = new Producto();
+
+            if (dataRow.Table.Columns.Contains("ID_PRODUCTO"))
+                entidad.Id = (int)dataRow["ID_PRODUCTO"];
+
+            if (dataRow.Table.Columns.Contains("DESCRIPCION"))
+                entidad.Descripcion = dataRow["DESCRIPCION"].ToString();
+
+            if (dataRow.Table.Columns.Contains("PRECIO"))
+                entidad.Precio = (int)dataRow["PRECIO"];
+
+            if (dataRow.Table.Columns.Contains("ID_CATEGORIA")) { 
+                entidad.IdCategoria = (int)dataRow["ID_CATEGORIA"];
+
+            }
+            if (dataRow.Table.Columns.Contains("DESCRIPCION_CATEGORIA")) { 
+                entidad.DescripcionCategoria = dataRow["DESCRIPCION_CATEGORIA"].ToString();
+            }
+
+            return entidad;
+        }
+    }
+}
