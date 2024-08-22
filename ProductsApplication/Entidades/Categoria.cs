abc5d4b0e7e3fcd042edcc9176767e01b3ef9063
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-08-22 18:09:37 -0300

    Added entity/category

---
+++ ProductsApplication/Entidades/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace Entidades
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        public Categoria FromDataRow(DataRow dataRow)
+        {
+            Categoria entidad = new Categoria();
+
+            if (dataRow.Table.Columns.Contains("ID_CATEGORIA"))
+                entidad.Id = (int)dataRow["ID_CATEGORIA"];
+
+            if (dataRow.Table.Columns.Contains("DESCRIPCION"))
+                entidad.Descripcion = dataRow["DESCRIPCION"].ToString();
+
+            return entidad;
+        }
+    }
+}
