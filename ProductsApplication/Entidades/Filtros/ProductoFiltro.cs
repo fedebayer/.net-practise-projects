6d3d4cd7a6d7af20d00f403b00747d0f5992e370
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-08-23 18:06:35 -0300

    Added entity/filter/productFilter

---
+++ ProductsApplication/Entidades/Filtros/ProductoFiltro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Filtros
+{
+    public class ProductoFiltro
+    {
+        public virtual string Descripcion { get; set; }
+
+        public virtual string Categoria { get; set; }
+
+        public virtual int? Id { get; set; }
+    }
+}
