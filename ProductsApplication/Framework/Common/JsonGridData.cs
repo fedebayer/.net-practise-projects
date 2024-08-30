3f2e97c954dcd08d7312af18794d6c847630ca27
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-08-30 18:16:12 -0300

    Added json grid data

---
+++ ProductsApplication/Framework/Common/JsonGridData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Framework.Common
+{
+    public class JsonGridData
+    {
+        public dynamic rows { get; set; }
+        public int total { get; set; }
+        public int records { get; set; }
+        public int page { get; set; }
+        public int rowNum { get; set; }
+    }
+}
