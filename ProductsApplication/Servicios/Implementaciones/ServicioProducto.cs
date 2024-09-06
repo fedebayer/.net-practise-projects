c4ce8906d0be4995a54ba4152a503378e4763fea
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-09-06 18:03:40 -0300

    Added product service

---
+++ ProductsApplication/Servicios/Implementaciones/ServicioProducto.cs
@@ -0,0 +1,51 @@
+﻿using Datos.Implementaciones;
+using Entidades;
+using Entidades.Filtros;
+using Framework.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Implementaciones
+{
+    public class ServicioProducto
+    {
+        DatosProducto _datos;
+        public ServicioProducto()
+        {
+            _datos = new DatosProducto();
+        }
+
+        public void Insertar(Producto p)
+        {
+            _datos.Insertar(p);
+        }
+
+        public void Actualizar(Producto p)
+        {
+            _datos.Actualizar(p);
+        }
+        
+        public void Borrar(int id)
+        {
+            _datos.Borrar(id);
+        }
+
+        public List<Producto> ObtenerPorFiltro(ProductoFiltro pFiltro)
+        {
+            return _datos.ObtenerPorFiltro(pFiltro);
+        }
+
+        public List<Producto> ObtenerPaginado(string descripcion, string categoria, string sidx, string sord, int page, int rows, out int total)
+        {
+            return _datos.ObtenerPaginado(descripcion, categoria, sidx, sord, page, rows, out total);
+        }
+
+        public List<Categoria> ObtenerCategorias()
+        {
+            return _datos.ObtenerCategorias();
+        }
+    }
+}
