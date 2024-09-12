31c2694fa63c80e922952b7d7b9f9866ce3d567c
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-09-12 19:03:21 -0300

    Added grid view model

---
+++ ProductsApplication/Integrador.Web/ViewModels/GridViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Integrador.Web.ViewModels
+{
+    public class GridViewModel
+    {
+        public string sidx { get; set; }
+        public string sord { get; set; }
+        public int page { get; set; }
+        public int rows { get; set; }
+
+        public GridViewModel()
+        {
+            this.sidx = "";
+            this.sord = "";
+            this.page = 0;
+            this.rows = 0;
+        }
+    }
+}
