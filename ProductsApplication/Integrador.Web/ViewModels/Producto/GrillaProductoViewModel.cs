5f5322c436108c8b1fa0974aae0ec44d6ced0fd3
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-09-13 18:25:08 -0300

    Added product grid viewmodel

---
+++ ProductsApplication/Integrador.Web/ViewModels/Producto/GrillaProductoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Integrador.Web.ViewModels.Producto
+{
+    public class GrillaProductoViewModel
+    {
+        public List<ItemViewModel> Items { get; set; }
+
+        public class ItemViewModel
+        {
+            public string ID { get; set; }
+            public string Descripcion { get; set; }
+            public int Precio { get; set; }
+
+            public int IdCategoria { get; set; }
+
+            public string DescripcionCategoria { get; set; }
+            public string FechaAlta { get; set; }
+            public bool Activo { get; set; }
+        }
+
+    }
+}
