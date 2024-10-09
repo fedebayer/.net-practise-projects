3ee2f417c1a8efa24a0e479f22941764863d90b5
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-10-09 18:32:47 -0300

    Added products table in script.sql

--- ShoppingCartApplication/Scripts.sql
+++ ShoppingCartApplication/Scripts.sql
@@ -84,3 +84,26 @@ CREATE TABLE [dbo].[ShoppingCartData](
 GO
 SET ANSI_PADDING OFF
 GO
+/****** Object:  Table [dbo].[Products]    Script Date: 09/10/2024 18:24:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Products](
+	[PID] [int] IDENTITY(1,1) NOT NULL,
+	[PName] [varchar](50) NOT NULL,
+	[Brand] [varchar](50) NOT NULL,
+	[UnitPrice] [money] NOT NULL,
+	[UnitsInStock] [int] NOT NULL,
+	[Category] [varchar](50) NOT NULL,
+	[Description] [varchar](50) NOT NULL,
+	[SID] [int] NOT NULL,
+	[ROL] [int] NOT NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[PID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
